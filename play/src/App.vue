4962c95d64eae75daa985bcb8342dcc6ca0d88aa
IceyWu <3128006406@qq.com>
2024-05-08 18:08:39 +0800

    feat: [log]add consolePlus

--- play/src/App.vue
+++ play/src/App.vue
@@ -1,42 +1,40 @@
 <template>
-  <div font-sans text="center gray-700 dark:gray-200" class="w-full h-screen flex flex-col">
+  <div
+    font-sans
+    text="center gray-700 dark:gray-200"
+    class="w-full h-screen flex flex-col"
+  >
     <div class="sticky top-0">
       <TheHeader />
     </div>
     <div class="w-full box-border">
-      <button btn @click="download"> 文件下载</button>
-      <button btn @click="removeEmptyValuesFunc"> 空值移除</button>
+      <button btn @click="download">文件下载</button>
+      <button btn @click="removeEmptyValuesFunc">空值移除</button>
     </div>
   </div>
 </template>
 <script setup lang="ts">
-import { list } from "../../dist";
+import { list, consolePlus } from "../../src";
 
 onMounted(() => {
-
+  consolePlus.log('warning', 'test')
+  consolePlus.error("error", "error test", "[Error]: ");
 });
 
-
 const download = () => {
-  const fileUrl = 'https://www.douyin.com/aweme/v1/play/?video_id=v0200fg10000cnkpuf3c77u23t3ih4t0&line=0&file_id=70959f5972454016b94de0e1809adc31&sign=7227007acaae56f6652ae98985e3e561&is_play_url=1&source=PackSourceEnum_AWEME_DETAIL'
-  const fileName = 'test.mp4'
+  const fileUrl =
+    "https://www.douyin.com/aweme/v1/play/?video_id=v0200fg10000cnkpuf3c77u23t3ih4t0&line=0&file_id=70959f5972454016b94de0e1809adc31&sign=7227007acaae56f6652ae98985e3e561&is_play_url=1&source=PackSourceEnum_AWEME_DETAIL";
+  const fileName = "test.mp4";
 
-  const link = document.createElement('a');
+  const link = document.createElement("a");
   link.href = fileUrl;
   link.download = fileName;
   link.target = "_blank"; // 可选，如果希望在新窗口中下载文件，请取消注释此行
   link.click();
-
-}
-const removeEmptyValuesFunc = () => {
- 
 };
-onMounted(()=>{
-  const newList = list(0,10,'a')
- console.log('🍪-----newList-----', newList);
-})
-
-
-
-
+const removeEmptyValuesFunc = () => {};
+onMounted(() => {
+  const newList = list(0, 10, "a");
+  console.log("🍪-----newList-----", newList);
+});
 </script>
--- src/log/src/log.ts
+++ src/log/src/log.ts
@@ -1,38 +1,60 @@
-import colors from 'ansi-colors'
-import type { ILOG } from '../types'
+export const typeColor = (type = "default") => {
+  const baseColor = {
+    color: "black",
+    bg: "linear-gradient(120deg, #84fab0 0%, #8fd3f4 100%)",
+  };
+  switch (type) {
+    case "primary":
+      baseColor["color"] = "#fff";
+      baseColor["bg"] = "linear-gradient(to top, #209cff 0%, #68e0cf 100%)";
+      break;
+    case "success":
+      baseColor["color"] = "#fff";
+      baseColor["bg"] = "linear-gradient(to right, #92fe9d 0%, #00c9ff 100%)";
+      break;
+    case "info":
+      baseColor["color"] = "#fff";
+      baseColor["bg"] = "linear-gradient(120deg, #a6c0fe 0%, #f68084 100%)";
+      break;
+    case "warning":
+      baseColor["color"] = "#fff";
+      baseColor["bg"] = "linear-gradient(-60deg, #ff5858 0%, #f09819 100%)";
+      break;
+    case "danger":
+      baseColor["color"] = "#fff";
+      baseColor["bg"] =
+        "linear-gradient(to right, #f78ca0 0%, #f9748f 19%, #fd868c 60%, #fe9a8b 100%)";
+      break;
+    case "default":
+      baseColor["color"] = "#fff";
+      baseColor["bg"] = "linear-gradient(120deg, #e2e3e5 0%, #e2e3e5 100%)";
+      break;
+    default:
+      break;
+  }
+  return baseColor;
+};
 
-/**
- *  打印类，分为四中类型
- *  @internal
- */
-export const logType = {
-  info: (msg: string, prefix = '') => colors.blueBright.bold(`${prefix}${msg}`),
-  error: (msg: string, prefix = '') => colors.redBright.bold(`${prefix}${msg}`),
-  warning: (msg: string, prefix = '') => colors.yellowBright.bold(`${prefix}${msg}`),
-  success: (msg: string, prefix = '') => colors.greenBright.bold(`${prefix}${msg}`),
-}
+const logFunc = (type = "primary", title: string, ...contents: any[]) => {
+  const { color, bg } = typeColor(type);
+  console.group(
+    `%c ${title} `,
+    `background-image:${bg};color:${color}; padding: 2px; border-radius: 4px;font-size:14px;`
+  );
+  contents.forEach((content) => {
+    console.log(content[0]);
+  });
+  console.groupEnd();
+};
 
-/**
- * 全局打印前缀
- * @internal
- */
-export const globalPrefix = Symbol('Global Prefix Key')
-
-/**
- * 设置打印的全局前缀
- * @param prefix 全局前缀
- */
-export function setGlobalPrefix(prefix: string) {
-  (globalThis as any)[globalPrefix] = prefix
-}
-
-/**
- * 打印日志函数
- * @param type 打印输出类别
- * @param msg 打印输出信息
- * @param prefix 打印输出前缀，默认全局前缀
- */
-export function ILog(type: ILOG, msg: string, prefix = (globalThis as any)[globalPrefix] as string) {
-  // eslint-disable-next-line no-console
-  console.log(logType[type](msg, prefix))
-}
+export const consolePlus = {
+  log: (title: string, ...contents: any[]) =>
+    logFunc("default", title, contents),
+  error: (title: string, ...contents: any[]) =>
+    logFunc("danger", title, contents),
+  warn: (title: string, ...contents: any[]) =>
+    logFunc("warning", title, contents),
+  info: (title: string, ...contents: any[]) => logFunc("info", title, contents),
+  success: (title: string, ...contents: any[]) =>
+    logFunc("success", title, contents),
+};
