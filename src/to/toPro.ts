d31aa36a8787bc8d4762c2e1eec551f80891365d
IceyWu <kui.wu@boran-tech.com>
2024-07-03 16:53:32 +0800

    feat: [toPro] 新增toPro方法

---
+++ src/to/toPro.ts
@@ -0,0 +1,37 @@
+import { getObjVal } from '../object'
+import { isArray } from '../is'
+import { to } from './index'
+
+interface ValItem {
+  keys: string[]
+  valFormat?: any
+}
+
+export async function toPro(promise: Promise<T>, valList?: ValItem[]) {
+  const [err, res] = await to(promise)
+  if (err)
+    return [err, undefined]
+
+  if (isArray(valList)) {
+    const resObj = res as any
+    const dataList = [] as any
+    valList.forEach(({ keys, valFormat }) => {
+      const valList = keys.map((key) => {
+        return getObjVal(resObj, key)
+      })
+      const tempVal = valFormat ? valFormat(valList) : valList
+      if (isArray(tempVal) && tempVal.length === 1 && !valFormat)
+        dataList.push(tempVal[0])
+      else
+        dataList.push(tempVal)
+    })
+    return [undefined, dataList]
+  }
+  else {
+    return [undefined, res]
+  }
+}
+
+export default {
+  toPro,
+}
